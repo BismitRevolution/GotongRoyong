7f0e9deebccabbc98e77e87f0f3d5425cec0c90c
cymon1997 <cybermonster82@yahoo.com>
2018-09-09 03:40:15 +0700

    added working drawer

--- app/src/main/java/in/gotongroyong/gotongroyong/MainScreen.java
+++ app/src/main/java/in/gotongroyong/gotongroyong/MainScreen.java
@@ -1,14 +1,24 @@
 package in.gotongroyong.gotongroyong;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 import in.gotongroyong.gotongroyong.adapter.MainPageAdapter;
 
 public class MainScreen extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -16,10 +26,78 @@ public class MainScreen extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_action_hamburger);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView nav = findViewById(R.id.drawer_navigation);
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                drawer.closeDrawers();
+                return true;
+            }
+        });
+
+        drawer.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+
         MainPageAdapter adapter = new MainPageAdapter(getSupportFragmentManager());
-        ViewPager pager = findViewById(R.id.main_pager);
+        final ViewPager pager = findViewById(R.id.main_pager);
+        final TabLayout tab = findViewById(R.id.main_tab);
+        tab.setupWithViewPager(pager);
         pager.setAdapter(adapter);
 
+        tab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 //        startActivity(new Intent(this, TestActivity.class));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
