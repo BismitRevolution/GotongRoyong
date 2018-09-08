88ca8f913949a7c007119e5761f73b83d3dcb406
cymon1997 <cybermonster82@yahoo.com>
2018-09-08 20:54:53 +0700

    added loading screen

---
+++ app/src/main/java/in/gotongroyong/gotongroyong/LoadingScreen.java
@@ -0,0 +1,37 @@
+package in.gotongroyong.gotongroyong;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoadingScreen extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.screen_loading);
+
+        Thread loading = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    finishLoading();
+                } finally {
+                    finishLoading();
+                }
+            }
+        };
+
+        loading.start();
+    }
+
+    private void finishLoading() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
